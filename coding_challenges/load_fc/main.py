9d287fbc60f666418bc70065c49c7c3c9dd4f3a8
silverjoda <heizen.sh@gmail.com>
2022-10-05 15:48:34 +0200

    Finish stl forecasting + baseline + hyperparam optimization method

--- coding_challenges/load_fc/main.py
+++ coding_challenges/load_fc/main.py
@@ -19,7 +19,47 @@ from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tsa.forecasting.stl import STLForecast
 from statsmodels.tsa.exponential_smoothing.ets import ETSModel
 from statsmodels.tsa.ar_model import AutoReg
+from statsmodels.tools.eval_measures import mse, rmse, rmspe, meanabs, medianbias
+import optuna
+import random
 
+class STLClassifierWrapper:
+    def __init__(self, param_dict):
+        self.stl_model = None
+        self.stlf_res = None
+        self.param_dict = param_dict
+
+    def fit(self, trn_data):
+        self.stl_model = STLForecast(trn_data,
+                                     ARIMA,
+                                     model_kwargs=dict(order=self.param_dict["arima_order"], trend="t"),
+                                     period=48,  # 48
+                                     seasonal=self.param_dict["seasonal"],  # 3,7
+                                     trend_deg=self.param_dict["trend_deg"])  # 1
+        self.stlf_res = self.stl_model.fit()
+
+    def forecast(self, steps):
+        if self.stlf_res is None:
+            raise Exception("Model not fitted yet")
+        return self.stlf_res.forecast(steps)
+
+class SameAsYesterdayForecaster:
+    def __init__(self):
+        pass
+
+    def fit(self, trn_data):
+        self.trn_data = trn_data
+
+    def forecast(self, n_steps):
+        # Return n_steps of the trn data minus 48 settlement periods
+        last_idx = self.trn_data.keys()[-1]
+        # Pick the correct segment which corresponds to yesterday's segment
+        forecasted_data = self.trn_data.loc[last_idx-48:last_idx-48+n_steps-1]
+
+        # Change the indeces to reflect the future data (for plotting)
+        forecasted_data_shifted = pd.Series(data=forecasted_data.values, index=forecasted_data.index + 48)
+
+        return forecasted_data_shifted
 
 def read_elexon(date_from, date_to, series_code):
     # Time delta of 1 day. We'll use this to iterate over the dates
@@ -176,44 +216,154 @@ def analyze_dataset(filename):
     #
     # plt.show()
 
-    # Split data into train (march-august) and test (september)
-    # print(pd.to_datetime(df["settlement_date"], infer_datetime_format=True).info())
-    # exit()
+    param_dict = get_default_param_dict()
+    stl_model = STLClassifierWrapper(param_dict)
+    say_model = SameAsYesterdayForecaster()
+    #print(evaluate_forecasting_model(df, param_dict, stl_model))
+    #print(evaluate_forecasting_model_full(df, param_dict, say_model))
+    #exit()
 
-    # Normalize the data
+    #optimize_stl_forecasting_model(df)
 
-    # Analyze data in the frame of a week (month):
-    # Decompose the data into trend, seasonality and noise
-    # Perform fourier analysis with the iterative noise rejection method
-    # Try SARIMAX
-    # Try Ready sota method such as Prophet
+    #mean_err = evaluate_stl_forecasting_model(df, get_default_param_dict())
+    #print(mean_err)
+    #exit()
 
     # Analyze all the data
     # Try LSTM or similar NN on whole dataset with several days of history and 12h horizon
     # Try incorporate exogenous variables such as information from other available series in the API
 
     start_date = datetime.datetime(2022, 3, 1)
-    end_date = datetime.datetime(2022, 3, 2)
-    val_start_date = datetime.datetime(2022, 3, 3)
-    val_end_date = datetime.datetime(2022, 3, 5)
+    end_date = datetime.datetime(2022, 3, 7)
+    val_start_date = datetime.datetime(2022, 3, 8)
+    val_end_date = datetime.datetime(2022, 3, 8)
     trn_df = df.loc[(df["settlement_date"] >= start_date) &
                     (df["settlement_date"] <= end_date)]
     val_df = df.loc[(df["settlement_date"] >= val_start_date) &
                     (df["settlement_date"] <= val_end_date)]
 
+    tic = time.perf_counter()
+
     # TODO: Add exog variables to arima
-    stlf = STLForecast(trn_df["quantity"], ARIMA, model_kwargs=dict(order=(1, 1, 0), trend="t"), period=48, seasonal=3, trend_deg=1)
+    #quantity_arr = np.array(trn_df["quantity"].values)
+    stlf = STLForecast(trn_df["quantity"], ARIMA, model_kwargs=dict(order=(1, 1, 0), trend="t", exog=trn_df["quantity"]), period=48, seasonal=3, trend_deg=1)
     stlf_res = stlf.fit()
 
-    print(stlf_res.summary())
+    toc = time.perf_counter()
+    print(f"Done in {toc - tic:0.4f} seconds")
+
+    #print(stlf_res.summary())
 
     # TODO: Add evaluation
 
-    forecast = stlf_res.forecast(48)
+    forecast = stlf_res.forecast(48, exog=val_df["quantity"])
     plt.plot(trn_df["quantity"], 'b')
     plt.plot(val_df["quantity"], 'g')
     plt.plot(forecast, 'r--')
     plt.show()
+
+def get_default_param_dict():
+    param_dict = {}
+    param_dict["lossfun"] = rmse
+    param_dict["n_lag_days"] = 3
+    param_dict["seasonal"] = 7
+    param_dict["trend_deg"] = 1
+    param_dict["arima_order"] = (1, 0, 0)
+    return param_dict
+
+def evaluate_forecasting_model_full(df, param_dict, model):
+    # Get quantitative evaluation
+    #score = evaluate_forecasting_model(df, param_dict, model)
+    #print(f"Model evaluation score on test set: {score}, according to loss function: {param_dict['lossfun']}")
+
+    # Get qualitative evaluation
+    base_idx = df.loc[df["settlement_date"] == datetime.datetime(2022, 9, 1)].index[0]
+    n_evals = 5
+
+    f = plt.figure(figsize=(6, 6))
+    gs = f.add_gridspec(1, n_evals)
+    for i in range(n_evals):
+        rnd_idx_shift = np.random.randint(1, 15) * 48
+        trn_df = df.loc[base_idx - param_dict["n_lag_days"] * 48 + rnd_idx_shift:base_idx + rnd_idx_shift]
+        val_df = df.loc[base_idx + rnd_idx_shift:base_idx + rnd_idx_shift + 23]
+
+        model.fit(trn_df["quantity"])
+        forecast = model.forecast(24)
+        with sns.axes_style("darkgrid"):
+            ax = f.add_subplot(gs[0, i])
+            plt.plot(trn_df["quantity"], 'b')
+            plt.plot(val_df["quantity"], 'g')
+            plt.plot(forecast, 'r--')
+
+    plt.show()
+
+def evaluate_forecasting_model(df, param_dict, model):
+    '''
+    Return the evaluation of the forecasting model on the whole test set
+    :param param_dict:
+    :return:
+    '''
+
+    n_lag_sp = param_dict["n_lag_days"] * 48 # Amount of settlement periods that model is being conditioned on
+    n_eval_sp = 24 # Amount of settlement periods to evaluate the model on (12 hours)
+    settlement_period_skip = 8 # Amount of settlement periods to skip between each evaluation (to reduce computational cost)
+
+    # Iterate over each day in september (test month)
+    cum_error = 0  # Cumulative error of the model over the entire test set
+    eval_ctr = 0
+    for i in range(1, 30):
+        # Get index of the first settlement period of the day for this date
+        base_idx = df.loc[df["settlement_date"] == datetime.datetime(2022, 9, i)].index[0]
+
+        for j in range(1, 48, settlement_period_skip):
+            trn_df = df.loc[base_idx - n_lag_sp + j:base_idx + j]
+            val_df = df.loc[base_idx + j:base_idx + j + 23]
+
+            # Make the model and fit
+            # stlf = STLForecast(trn_df["quantity"],
+            #                    ARIMA,
+            #                    model_kwargs=dict(order=param_dict["arima_order"], trend="t"),
+            #                    period=48, # 48
+            #                    seasonal=param_dict["seasonal"], # 3,7
+            #                    trend_deg=param_dict["trend_deg"]) # 1
+            # stlf_res = stlf.fit()
+            model.fit(trn_df["quantity"])
+
+            # Evaluate forecast
+            #cum_error += param_dict["lossfun"](val_df["quantity"], stlf_res.forecast(n_eval_sp))
+            cum_error += param_dict["lossfun"](val_df["quantity"], model.forecast(n_eval_sp))
+            eval_ctr += 1
+
+    return cum_error / eval_ctr
+
+def optimize_forecasting_model(df):
+    '''
+    Return dict of optimal parameters and optimal cost function
+    :return:
+    '''
+
+    def objective(df, trial):
+        param_dict = {}
+        param_dict["lossfun"] = rmse
+        param_dict["n_lag_days"] = trial.suggest_int("n_lag_days", 1, 14)
+        param_dict["seasonal"] = trial.suggest_categorical("seasonal", [3, 7, 9])
+        param_dict["trend_deg"] = trial.suggest_categorical("trend_deg", [1, 2, 3])
+        #ao_0 = trial.suggest_int("arima_order", 1, 3)
+        #ao_1 = trial.suggest_int("arima_order", 1, 3)
+        #ao_2 = trial.suggest_int("arima_order", 1, 3)
+        #param_dict["arima_order"] = (ao_0, ao_1, ao_2)
+        param_dict["arima_order"] = (1, 0, 0)
+
+        score = evaluate_forecasting_model(df, param_dict)
+
+        return score
+
+    timeout = 60.0
+    print(f"Running optimization for {timeout} seconds...")
+    study = optuna.create_study(direction="minimize")
+    study.optimize(lambda t: objective(df, t), timeout=timeout)
+
+    print(f"Best value is : {study.best_value}, with param vec: {study.best_params}")
 
 def date_test():
     # consider the start date as 2021-march 1 st
@@ -244,7 +394,6 @@ def main():
     #read_elexon(datetime.date(2022, 3, 1), datetime.date(2022, 9, 30), SERIES_CODE_LOAD)
     analyze_dataset("data/series_B0610.xml")
     #testpd()
-
 
 
 if __name__ == "__main__":
--- draft/main.py
+++ draft/main.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
 
-arr = np.array([[1, 2], [4, 5], [7, 8]])
-print(arr.shape)
-df2 = pd.DataFrame(arr,columns=['a', 'b', 'c'])
+arr = np.array([[1, 2], [4, 5], [7, 8], [5, 2], [1, 1]])
+df = pd.DataFrame(arr,columns=['a', 'b'])
+
+data_ser = df['a']
+print(data_ser)
+data_ser = pd.Series(data=data_ser.values, index=data_ser.index + 10)
+print(data_ser)
